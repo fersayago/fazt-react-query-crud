0fe7a7d200b2532cc54199dbf1d7c1669454b455
fersayago <sayago.fernando@gmail.com>
2022-11-21 17:25:30 -0300

    useMutation - DELETE PUT

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,11 +1,9 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createProduct } from '../api/productsAPI'
+import { createProduct } from "../api/productsApi";
 
 const ProductForm = () => {
-
   const queryClient = useQueryClient();
 
-  // a diferencia de la queries, las mutaciones generalmente se utilizan para crear/actualizar/eliminar data o que el server ejecute tareas en segundo plano. React Query utiliza un hook useMutation
   const addProductMutation = useMutation({
     mutationFn: createProduct,
     onSuccess: () => {
@@ -22,24 +20,19 @@ const ProductForm = () => {
       ...product,
       inStock: true,
     })
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor='name'>Name</label>
-      <input type='text' id='name' name='name'/>
-
-      <label htmlFor='description'>Description</label>
-      <input type='text' id='description' name='description'/>
-
-      <label htmlFor='price'>Price</label>
-      <input type='number' id='price' name='price'/>
-
-      <button>
-        Add Product
-      </button>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
+      <label htmlFor="price">Price</label>
+      <input type="number" name="price" id="price" />
+      <label htmlFor="description">Description</label>
+      <textarea name="description" id="description" />
+      <button type="submit">Submit</button>
     </form>
-  )
-}
+  );
+};
 
-export default ProductForm
+export default ProductForm;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,26 +1,66 @@
-import { useQuery } from '@tanstack/react-query'
-import { getProducts } from "../api/productsAPI"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteProduct, getProducts, updateProduct } from "../api/productsApi";
 
 const Products = () => {
-  const { isLoading, data: products, isError, error } = useQuery({
-    queryKey: ['products'],
+  const {
+    isLoading,
+    data: products,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["products"],
     queryFn: getProducts,
-    select: products => products.sort((a, b) => b.id - a.id)
-  })
-  
-  if (isLoading) return <div>Loading...</div>
-  else if (isError) return <div>Error: {error.message}</div>
+    select: (data) => data.sort((a, b) => b.id - a.id),
+  });
+  const queryClient = useQueryClient();
 
-  return products.map(item => (
-    <div key={ item.id }>
-      <h3>{ item.name }</h3>
-      <p>{ item.description }</p>
-      <p>{ item.price }</p>
-      <button>Delete</button>
-      <input type='checkbox' />
-      <label htmlFor="">In Stock</label>
+  const deleteProductMutation = useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries("products");
+    },
+  });
+
+  const updateProductMutation = useMutation({
+    mutationFn: updateProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries("products");
+    },
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  else if (isError) return <div>{error.message}</div>;
+
+  return products.map((product) => (
+    <div key={product.id}>
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <button
+        onClick={() => {
+          deleteProductMutation.mutate(product.id);
+        }}
+      >
+        Delete
+      </button>
+      <input
+        type="checkbox"
+        checked={product.inStock}
+        id={product.id}
+        onChange={(e) => {
+          console.log({
+            ...product,
+            inStock: e.target.checked,
+          });
+          updateProductMutation.mutate({
+            ...product,
+            inStock: e.target.checked,
+          });
+        }}
+      />
+      <label htmlFor={product.id}>In Stock</label>
     </div>
-  ))
-}
+  ));
+};
 
-export default Products
+export default Products;
